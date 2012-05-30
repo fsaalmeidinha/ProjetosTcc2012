192cd6b5442d5e8bb86300688981f5f7bed85cce
wagner <wagnernakamura@yahoo.com.br>
2012-05-29 22:44:34 -0300

    CotacaoDolar

--- Indices/CurrencyReader/CurrencyReader/Program.cs
+++ Indices/CurrencyReader/CurrencyReader/Program.cs
@@ -9,7 +9,27 @@ namespace CurrencyReader
     {
         static void Main(string[] args)
         {
-            WebRequests.LerCotacao("");
+            string valores = WebRequests.LerCotacao();
+
+            try
+            {
+
+                valores = valores.Replace(',', '.');
+                List<String> cotacoes = valores.Split(new char[] { ' ', '\n' }).ToList(); ;
+                cotacoes.RemoveAll(d => d.Contains('\r'));
+                
+                for (int i = 0; i < cotacoes.Count - 1; i += 2)
+                {
+                    DateTime data = Convert.ToDateTime(cotacoes[i]);
+
+                    WebRequests.SalvarCotacao(data, Convert.ToDecimal(cotacoes[i + 1], System.Globalization.CultureInfo.GetCultureInfo("en-US")));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
         }
     }
 }
--- Indices/CurrencyReader/CurrencyReader/WebRequests.cs
+++ Indices/CurrencyReader/CurrencyReader/WebRequests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace CurrencyReader
 {
@@ -23,6 +24,42 @@ namespace CurrencyReader
             }
 
             return sb.ToString();
+        }
+
+        public static void SalvarCotacao(DateTime data, decimal valor)
+        {
+            SqlConnection conn = null;
+            SqlCommand sqlComan = null;
+
+            try
+            {
+                conn = new SqlConnection(@"Data Source=WAGNER-PC\SQLEXPRESS;Initial Catalog=FinanceInvest;Integrated Security=True");
+                conn.Open();
+
+                sqlComan = new SqlCommand("Insert into CotacaoDolar(data, valor) values(@data, @valor)", conn);
+                sqlComan.CommandType = System.Data.CommandType.Text;
+
+                sqlComan.Parameters.Add("@data", System.Data.SqlDbType.SmallDateTime);
+                sqlComan.Parameters["@data"].Value = data;
+                sqlComan.Parameters.Add("@valor", System.Data.SqlDbType.Decimal);
+                sqlComan.Parameters["@valor"].Value = valor;
+
+                sqlComan.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (sqlComan != null)
+                    sqlComan.Dispose();
+                if (conn != null)
+                {
+                    conn.Close();
+                    conn.Dispose();
+                }
+            }
         }
     }
 }
