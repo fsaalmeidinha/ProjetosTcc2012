0b25feccec6e76cddd76e307352254b78c870062
Felipe Almeida <fsaalmeidinha@hotmail.com>
2012-08-06 20:43:10 -0300

    Estacao do ano

--- Redes/DataBaseUtils/DataBaseUtils/DataBaseUtils.cs
+++ Redes/DataBaseUtils/DataBaseUtils/DataBaseUtils.cs
@@ -33,7 +33,7 @@ namespace DataBaseUtils
                     cotacao.PrecoAbertura = (decimal)dtr["precoabertura"];
                     cotacao.PrecoAberturaNormalizado = (decimal)dtr["precoaberturaNormalizado"];
                     cotacao.CotacaoDolar = (decimal)dtr["valorDolar"];
-                    //cotacao.EstacaoDoAno = RecuperaEstacaoDoAno(cotacao.DataGeracao);
+                    cotacao.EstacaoDoAno = RecuperarEstacaoDoAno(cotacao.DataGeracao);
 
                     listCotacoes.Add(cotacao);
                 }
@@ -84,62 +84,44 @@ namespace DataBaseUtils
 
         }
 
-        public static double NormalizarDado(double dado, string papel)
+        private static double RecuperarEstacaoDoAno(DateTime data)
         {
-            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
-            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
+            return data.Subtract(new DateTime(data.Year, 1, 1)).TotalDays / 365.0;
+            //dynamic datasEstacoes = new
+            //{
+            //    dataInicialOutono = new DateTime(data.Year, 3, 20),
+            //    dataFinalOutono = new DateTime(data.Year, 6, 19),
+            //    dataInicialInverno = new DateTime(data.Year, 6, 20),
+            //    dataFinalInverno = new DateTime(data.Year, 9, 21),
+            //    dataInicialPrimavera = new DateTime(data.Year, 9, 22),
+            //    dataFinalPrimavera = new DateTime(data.Year, 12, 21),
+            //    dataInicialVerao = new DateTime(data.Year, 12, 22),
+            //    dataFinalVerao = new DateTime(data.Year + 1, 3, 19)
+            //};
 
-            return (dado - min) / (max - min);
+            ////OUTONO
+            //if (DateTime.Compare(data, datasEstacoes.dataInicialOutono) >= 0 && DateTime.Compare(data, datasEstacoes.dataFinalOutono) <= 0)
+            //{
+            //    return 0;
+            //}
+            ////INVERNO
+            //else if (DateTime.Compare(data, datasEstacoes.dataInicialInverno) >= 0 && DateTime.Compare(data, datasEstacoes.dataFinalInverno) <= 0)
+            //{
+
+            //}
+            ////PRIMAVERA
+            //else if (DateTime.Compare(data, datasEstacoes.dataInicialPrimavera) >= 0 && DateTime.Compare(data, datasEstacoes.dataFinalPrimavera) <= 0)
+            //{
+
+            //}
+            ////VERÃO
+            //else if (DateTime.Compare(data, datasEstacoes.dataInicialVerao) >= 0 && DateTime.Compare(data, datasEstacoes.dataFinalVerao) <= 0)
+            //{
+
+            //}
         }
 
-        public static List<double> NormalizarDados(List<double> dados, string papel)
-        {
-            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
-            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
-
-            List<double> dadosList = new List<double>();
-            dados.ForEach(dado => dadosList.Add((dado - min) / (max - min)));
-            return dadosList;
-        }
-
-        public static double DesnormalizarDado(double dado, string papel)
-        {
-            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
-            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
-
-            return dado * (max - min) + min;
-        }
-
-        public static List<double> DesnormalizarDados(List<double> dados, string papel)
-        {
-            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
-            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
-
-            List<double> dadosList = new List<double>();
-            dados.ForEach(dado => dadosList.Add(dado * (max - min) + min));
-            return dadosList;
-        }
-
-        ///// <summary>
-        ///// Retorna uma lista de KeyValuePair, onde a chave são os valores de input e o seu valor correspondente são os valores de output
-        ///// </summary>
-        ///// <param name="dados"></param>
-        ///// <param name="janelaEntrada">tamanho do input</param>
-        ///// <param name="janelaSaida">tamanho do output</param>
-        ///// <param name="considerarSaidasComoEntradas">se verdadeiro, teremos 'dados.Count / janelaEntrada' registros como saida, caso contrario, 'dados.Count / (janelaEntrada + janelaSaida)' </param>
-        ///// <returns></returns>
-        //public static List<KeyValuePair<double[], double[]>> SelecionarCotacoesPorJanelamento(List<double> dados, int janelaEntrada, int janelaSaida, bool considerarSaidasComoEntradas)
-        //{
-        //    /*Cria um mapeamento de entradas para saida com o janelamento informado*/
-        //    List<KeyValuePair<double[], double[]>> dadosPorJanelamento = new List<KeyValuePair<double[], double[]>>();
-        //    for (int i = 0; i < dados.Count - (janelaEntrada + janelaSaida); i += janelaEntrada + (considerarSaidasComoEntradas ? 0 : janelaSaida))
-        //    {
-        //        dadosPorJanelamento.Add(new KeyValuePair<double[], double[]>(dados.Skip(i).Take(janelaEntrada).ToArray(), dados.Skip(i + janelaEntrada).Take(janelaSaida).ToArray()));
-        //    }
-        //    /*Cria um mapeamento de entradas para saida com o janelamento informado*/
-
-        //    return dadosPorJanelamento;
-        //}
+        #region METODOS DE SELECAO DE DADOS
 
         /// <summary>
         /// Retorna uma lista de Treinamento com seus devidos inputs e outputs e o seu shift correspondente
@@ -194,6 +176,10 @@ namespace DataBaseUtils
             return dadosPorJanelamento;
         }
 
+        #endregion METODOS DE SELECAO DE DADOS
+
+        #region DESDOBRAMENTO
+
         /// <summary>
         /// Trata os desdobramentos (verifica alterações de 50% no valor de um dia para o outro)
         /// </summary>
@@ -214,6 +200,50 @@ namespace DataBaseUtils
                 }
             }
         }
+
+        #endregion DESDOBRAMENTO
+
+        #region NORMALIZAÇÂO
+
+        public static double NormalizarDado(double dado, string papel)
+        {
+            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
+            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
+
+            return (dado - min) / (max - min);
+        }
+
+        public static List<double> NormalizarDados(List<double> dados, string papel)
+        {
+            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
+            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
+
+            List<double> dadosList = new List<double>();
+            dados.ForEach(dado => dadosList.Add((dado - min) / (max - min)));
+            return dadosList;
+        }
+
+        public static double DesnormalizarDado(double dado, string papel)
+        {
+            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
+            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
+
+            return dado * (max - min) + min;
+        }
+
+        public static List<double> DesnormalizarDados(List<double> dados, string papel)
+        {
+            double min = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMinimo" + papel.ToUpper()));
+            double max = Convert.ToDouble(Resources.ResourceManager.GetString("ValorMaximo" + papel.ToUpper()));
+
+            List<double> dadosList = new List<double>();
+            dados.ForEach(dado => dadosList.Add(dado * (max - min) + min));
+            return dadosList;
+        }
+
+        #endregion NORMALIZAÇÂO
+
+        #region Métodos para o acesso ao BD(Wagner)
 
         /// <summary>
         /// 
@@ -265,5 +295,7 @@ namespace DataBaseUtils
 
             return oConexao;
         }
+
+        #endregion Métodos para o acesso ao BD(Wagner)
     }
 }
